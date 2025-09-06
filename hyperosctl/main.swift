7cd12ba865657ac6d10fee3a4bc595a1159e1b1f
Alejandro Visiedo <alejandro.visiedo@gmail.com>
2025-09-07 00:36:58 +0200

    wip feat: cli skeleton
    Add the skeleton for the cli tools.
    
    Signed-off-by: Alejandro Visiedo <alejandro.visiedo@gmail.com>

---
+++ hyperosctl/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  hyperosctl
+//
+//  Created by Alejandro Visiedo on 16/8/25.
+//  Copyright © 2025 Apple. All rights reserved.
+//
+
+import Foundation
+
+func main() {
+    print("Hello, World!")
+}
